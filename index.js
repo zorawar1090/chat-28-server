1fbf50718f7449db5e626d3e36733c71524c5c2a
zorawar1090 <zorawarsingh9988@gmail.com>
2019-08-09 14:32:34 +0200

    Destructure message objects

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const express = require('express')
 const Sse = require('json-sse')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const Sequelize = require('sequelize')
 
-const messages = ['hello world']
+const databaseUrl = 'postgres://postgres:advertisements@localhost:5432/postgres'
 
-const sse = new Sse(messages)
+const db = new Sequelize(databaseUrl)
+
+db.sync({force: false})
+  .then(() => console.log('Database synced'))
+
+const Message = db.define(
+  'message',
+  {
+    text: Sequelize.STRING
+  }
+)
+
+const stream = new Sse()
 
 const app = express()
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
 
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
 
-app.get('/stream', sse.init)
+app.get('/stream', async (req,res ) => {
+  const messages = await Message.findAll()
+  const data = JSON.stringify(messages)
+  stream.updateInit(data)
+  stream.init(req,res)
+})
 
-app.post('/message', (req, res) => {
+app.post('/message', async (req, res) => {
   const { message } = req.body
-  messages.push(message)
   
-  sse.updateInit(messages)
-  sse.send(message)
+  const entity = await Message.create({text: message})
 
-  res.send(message)
+  const messages = await Message.findAll()
+
+  const data = JSON.stringify(messages)
+
+  stream.updateInit(data)
+  stream.send(data)
+
+  res.send(entity)
 })
 
 const port = process.env.PORT || 5000

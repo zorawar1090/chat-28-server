b55b59e882934dc1d9e9027273b215dcd5ee2d24
zorawar1090 <zorawarsingh9988@gmail.com>
2019-08-09 10:42:59 +0200

    Initialize sse

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const Sse = require('json-sse')
+const bodyParser = require('body-parser')
+
+const data = 'hello world'
+
+const sse = new Sse(data)
 
 const app = express()
+
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
+
+app.get('/stream', sse.init)
+
+app.post('/message', (req, res) => {
+  const { message } = req.body
+  sse.send(message)
+
+  res.send(message)
+})
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
